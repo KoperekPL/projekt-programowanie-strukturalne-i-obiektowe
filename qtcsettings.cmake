f87e627ceaf307638e3e0195a6dccff7be63b207
michalpawlakpaw <michal.pawlak.paw@gmail.com>
2026-05-16 15:32:39 +0200

    Animacja postaci, mapa, parę grafik, test budowy zamku

---
+++ qtcsettings.cmake
@@ -0,0 +1,3 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_PREFIX_PATH" "C:/Qt/6.11.0/mingw_64;C:/Users/micha/Documents/SFML-2.6.2-windows-gcc-13.1.0-mingw-64-bit/SFML-2.6.2" CACHE "PATH" "" FORCE)
